afb2b4b3e3cca4f48298ae83a91916632c4ac9aa
Surfista <a21905840@alunos.ulht.pt>
2023-04-05 12:55:28 +0100

    Update settings.py
    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -145,7 +145,7 @@ SHORT_DATE_FORMAT = 'd/m/Y'   # Formato para os tabelas
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('gestaugi/static'))]
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
