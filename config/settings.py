9f60198f4716eead452ca46c9e371382dc51d228
Surfista <a21905840@alunos.ulht.pt>
2023-04-05 14:27:32 +0100

    Update files
    Update files

--- config/settings.py
+++ config/settings.py
@@ -108,9 +108,6 @@ DATABASES = {
     "default": env.dj_db_url("DATABASE_URL")
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
