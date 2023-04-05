353343bcde5ef8feae993f777a59081f96c820f8
Surfista <a21905840@alunos.ulht.pt>
2023-04-05 18:15:55 +0100

    Update admin.py
    Update admin.py

--- gestaugi/admin.py
+++ gestaugi/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Socios, Lotes, Comparticipacoes, Assembleias, Despesas
+from .models import Socios, Lotes, Comparticipacoes, Assembleias, Despesas, Anuidades, Pagamentos, Presencas, Parametros, CodPostais
 
 # Register your models here.
 
@@ -8,4 +8,8 @@ admin.site.register(Lotes)
 admin.site.register(Comparticipacoes)
 admin.site.register(Assembleias)
 admin.site.register(Despesas)
-
+admin.site.register(Pagamentos)
+admin.site.register(Parametros)
+admin.site.register(Anuidades)
+admin.site.register(CodPostais)
+admin.site.register(Presencas)
