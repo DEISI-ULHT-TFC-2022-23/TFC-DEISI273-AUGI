38a88514bcd3c3ef2a954689a0b221afced97639
Surfista <a21905840@alunos.ulht.pt>
2023-04-05 14:33:08 +0100

    Update settings.py
    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from environs import Env
-import dj_database_url
+env = Env()
+env.read_env()
 
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
@@ -23,12 +24,8 @@ MESSAGE_TAGS = {
         messages.ERROR: 'alert-danger',
 }
 
-env = Env()
-env.read_env()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
